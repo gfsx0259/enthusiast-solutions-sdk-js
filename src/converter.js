722a87aa28dc2f3e9c7ba8cf777324df27ec1dfe
k.popov <k.popov@it.ecommpay.com>
2025-04-03 15:46:03 +0300

    Initial commit

--- src/converter.js
+++ src/converter.js
@@ -1,28 +1,24 @@
-'use strict';
+function reducer(obj, prefix = '', ignored = []) {
+  const ordered = {};
 
-module.exports = (obj) => {
-    return reducer(obj).slice(0, -1);
-};
+  Object.keys(obj).sort().forEach((key) => {
+    if (ignored.indexOf(key) === -1) {
+      ordered[key] = obj[key];
+    }
+  });
 
-function reducer(obj, prefix = '', level = 1) {
-    const ordered = {};
-    Object.keys(obj).sort().forEach(function (key) {
-        ordered[key] = obj[key];
-    });
-
-    return Object.entries(ordered).reduce((acc, [prop, value]) => {
-        if (level === 3) {
-            return acc + `${prefix}:${prop};`;
-        } else if (level > 3) {
-            return acc;
-        }
-        if (typeof value === 'object') {
-            if (Array.isArray(value)) {
-                return acc + reducer(value, prop, level + 1);
-            }
-            return acc + reducer(value, prefix ? `${prefix}:${prop}` : prop, level + 1);
-        }
-        if (typeof value === typeof true) value = value ? 1 : 0;
-        return acc + (prefix ? `${prefix}:${prop}:${value};` : `${prop}:${value};`);
-    }, '')
+  return Object.entries(ordered).reduce((acc, [prop, value]) => {
+    if (value === null) {
+      // eslint-disable-next-line no-param-reassign
+      value = '';
+    }
+    if (typeof value === 'object') {
+      return acc + reducer(value, prefix ? `${prefix}:${prop}` : prop, ignored);
+    }
+    // eslint-disable-next-line no-param-reassign
+    if (typeof value === typeof true) value = value ? 1 : 0;
+    return acc + (prefix ? `${prefix}:${prop}:${value};` : `${prop}:${value};`);
+  }, '');
 }
+
+module.exports = (obj, ignored = []) => reducer(obj, '', ignored).slice(0, -1);
